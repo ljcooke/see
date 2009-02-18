6f2404e84f77ce038d1cec90f6263a670227741b
Liam Cooke <liamcooke@gmail.com>
2009-02-18 02:05:15 +0000

    Package classifiers

--- setup.py
+++ setup.py
@@ -8,4 +8,14 @@ setup( name='see',
        author='Liam Cooke',
        author_email='liamcooke@gmail.com',
        url='http://github.com/inky/see/tree/master',
-       py_modules=['see'] )
+       py_modules=['see'],
+       classifiers=[
+           'Development Status :: 4 - Beta',
+           'Environment :: Console',
+           'Intended Audience :: Developers',
+           'License :: OSI Approved :: GNU General Public License (GPL)',
+           'Natural Language :: English',
+           'Operating System :: OS Independent',
+           'Programming Language :: Python',
+           'Topic :: Software Development',
+       ])
