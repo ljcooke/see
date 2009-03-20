0e9f5d7f8a35b6ebcc3481fb64e81cb546459ac3
Liam Cooke <liamcooke@gmail.com>
2009-03-20 01:00:46 +0000

    New project URL: http://inky.github.com/see/

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup( name='see',
        description='A human-readable alternative to dir',
        author='Liam Cooke',
        author_email='liamcooke@gmail.com',
-       url='http://github.com/inky/see/tree/master',
+       url='http://inky.github.com/see/',
        py_modules=['see'],
        classifiers=[
            'Development Status :: 4 - Beta',
