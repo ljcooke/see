f3912f1172a4ce1a8c3fb74e03f58d7264204a82
Liam Cooke <liamcooke@gmail.com>
2009-04-09 16:16:08 +0100

    added license blurb to source

--- see.py
+++ see.py
@@ -10,9 +10,20 @@ A human alternative to dir().
 Copyright (c) 2009 Liam Cooke
 http://inky.github.com/see/
 
-Licensed under the GNU General Public License v3.
-See COPYING for the full license text.
+Licensed under the GNU General Public License v3. {{{
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}}}
 """
 
 __author__ = 'Liam Cooke'
@@ -219,4 +230,4 @@ SYMBOLS = tuple(filter(lambda x: x[0], (
     (PY_300 and '__unicode__', 'unicode()'),
 )))
 
-# vim: expandtab tabstop=4 shiftround shiftwidth=4
+# vim: expandtab tabstop=4 shiftround shiftwidth=4 fdm=marker
