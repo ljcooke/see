56ba41ee40bcedbaa0910862301bc1827b2e9030
Liam Cooke <git@araile.com>
2016-04-21 19:56:19 +0100

    Update AUTHORS filename
    [ci skip]

--- see.py
+++ see.py
@@ -51,7 +51,7 @@ except ImportError:
 __all__ = ['see']
 
 __author__ = 'Liam Cooke'
-__contributors__ = 'See AUTHORS.md'
+__contributors__ = 'See AUTHORS.rst'
 __version__ = '1.2.0'
 __copyright__ = 'Copyright (c) 2009-2016 Liam Cooke'
 __license__ = 'BSD License'
